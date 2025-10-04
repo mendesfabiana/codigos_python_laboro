d389ba00a5de8becca8c818f85fee20d3f757e54
Fabiana Mendes <dev.fabianamendes@gmail.com>
2025-10-04 17:50:54 -0300

    comentário inserido

--- enquanto.py
+++ enquanto.py
@@ -3,11 +3,11 @@
 valor = 1 # inicializando a variável
 
 while valor <= 20:
-    print(valor,end='  ')
+    print(valor,end='  ') # end='' para deixar os números na mesma linha
 
     valor = valor + 1 # valor += 1
 print('\n')
-print('-'* 50)
+print('-'* 70)
 print('\n')
 
 
