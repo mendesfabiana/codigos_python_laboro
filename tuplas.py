5a5ac68c211e9ee90110e6435b7de06a19a52280
Fabiana Mendes <dev.fabianamendes@gmail.com>
2025-10-15 19:43:17 -0300

    trabalhando com tupla

---
+++ tuplas.py
@@ -0,0 +1,21 @@
+# Tupla - conjunto de dados
+# Criando uma tupla  - representado por parênteses ()
+# o range sempre cria uma lista numérica - range(0,4)
+# a tupla é imutável 
+
+frutas = ('banana','uva','morango','acerola')
+
+# print(type(frutas))
+
+print(frutas)
+
+frutas[2] = 'manga' # aqui teremos um erro, já que ela é imutável
+
+
+print(frutas[2])
+
+# EXIBIR TODAS AS FRUTAS
+
+for item in frutas:
+    print(item)
+
